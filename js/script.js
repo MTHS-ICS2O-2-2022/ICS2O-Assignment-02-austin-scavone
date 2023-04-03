857ca7869d6880d0375e939246ef12f656f26abc
austin <124389297+austin-scavone@users.noreply.github.com>
2023-04-03 16:43:25 +0000

    changed

--- js/script.js
+++ js/script.js
@@ -3,3 +3,22 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+
+'use strict'
+/**
+ * This function calculates area and perimeter of parallelogram.
+ */
+function calculate() {
+  // input
+  const length = parseInt(document.getElementById('length-of-parallelogram').value)
+  const width = parseInt(document.getElementById('width-of-parallelogram').value)
+
+  // process
+  const area = length * width
+  const perimeter = 2 * (length + width)
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+  document.getElementById('perimeter').innerHTML = 'Perimeter is: ' + perimeter + ' cm'
+}
